05caab7b8d2e53457d9453c14a9b4aba2ea13d28
Robber779 <2jrdavis779@gmail.com>
2025-03-02 21:38:50 -0500

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -45,4 +45,17 @@ public class Calc{
       return "num1: "+num1+"\nnum2: "+num2;
    }
    
+   public static double checkNumber(){
+      Scanner scan = new Scanner(System.in);
+      double num = 0;
+      if(scan.hasNextDouble()){num = scan.nextDouble();} //hasNextDouble is a boolean statement that checks if the next number is a double or not(does not have to include '.'
+      
+      else{
+         System.out.println("Invalid input. Please select a number.");
+         num = checkNumber();//Simple recursion, I believe it's called! Learned somewhat about this in highschool.
+      }
+      
+      return num;
+   }
+   
 }
